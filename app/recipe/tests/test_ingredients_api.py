c43a8f50de7b26e86d95e9cda5c79b12f09601df
Sxmmie <samuelumohjnr@gmail..com>
2019-07-25 18:41:56 +0100

    Refactored the test and Ingredient views

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -65,3 +65,23 @@ class PrivateIngredientApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)  # just one result for authenticated user
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredients_successful(self):
+        """Test create new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENT_URL, payload)
+
+        # test for existence
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingredients_invalid(self):
+        """Test create ingredient invalid"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,29 +8,26 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-    """Manage tags in the database"""
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    """Base viewset for user owned recipe sttributes"""
     authentication_classes = (TokenAuthentication,) # token authentication is used
     permission_classes = (IsAuthenticated,) # user is required to be authenticated
-    queryset = Tag.objects.all()    # queryset to return
-    serializer_classes = serializers.TagSerializer
 
-    def query_set(self):
+    def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
+    def perform_create(self):
         """Create a new tag"""
         serializer.save(user=self.request.user)
 
-
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-    """Manage Ingrediens in the database"""
-    authentication_classes = (TokenAuthentication,) # token authentication is used
-    permission_classes = (IsAuthenticated,) # user is required to be authenticated
-    queryset = Ingredient.objects.all()    # queryset to return
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
+    queryset = Tag.objects.all()    # queryset to return
     serializer_classes = serializers.TagSerializer
 
-    def query_set(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage Ingrediens in the database"""
+    queryset = Ingredient.objects.all()    # queryset to return
+    serializer_classes = serializers.TagSerializer
